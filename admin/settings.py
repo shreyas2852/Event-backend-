808951881fcf7bdc96fd0703353272b3b8f0b6c9
shreyas2852 <shreyasmavani@gmail.com>
2024-05-14 20:45:15 +0530

    update setting

--- admin/settings.py
+++ admin/settings.py
@@ -37,8 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'events',
     'corsheaders',
+    'events',
 ]
 
 MIDDLEWARE = [
@@ -147,3 +147,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Enable Debug Logging
+import logging
+logging.basicConfig(level=logging.DEBUG)
